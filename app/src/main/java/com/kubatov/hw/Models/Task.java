e27420f8258d690222ec9322b230911f92a29d58
Azim Kubatov <46606593+Azim87@users.noreply.github.com>
2019-01-27 19:57:23 +0600

    add task model

---
+++ app/src/main/java/com/kubatov/hw/Models/Task.java
@@ -0,0 +1,15 @@
+package com.kubatov.hw.Models;
+
+import java.util.Date;
+
+public class Task {
+
+
+    String title;
+    String description;
+    Boolean isDone;
+    Date creationData;
+    Date endData;
+
+
+}
